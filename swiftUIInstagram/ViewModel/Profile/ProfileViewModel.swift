27fdc0c72e131e34ab15b456e7960ccf4437972a
Your Name <you@example.com>
2023-06-13 00:33:54 -0700

    matched backend to frontend

--- swiftUIInstagram/ViewModel/Profile/ProfileViewModel.swift
+++ swiftUIInstagram/ViewModel/Profile/ProfileViewModel.swift
@@ -34,8 +34,16 @@ class ProfileViewModel: ObservableObject {
 
     
     func checkIfUserIsFollowed() {
-        
+        guard !user.isCurrentUser else {return}
+        guard let uid = user.id else {return}
+        UserService.checkIfUserIsFollowed(uid: uid) { isFollowed in
+            print("DEBUG:Successfully Unfollowed \(self.user.username) ..")
+            self.user.isFollowed = isFollowed
+        }
     }
+        //checkIfUserIsFollowed this view is to validate the cleint with the back end with a snapshot
+        //isFollowed:Bool is the completion block
+        //  guard !user.isCurrentUser else {return} make sure you don't follow current user
 }
 
 
