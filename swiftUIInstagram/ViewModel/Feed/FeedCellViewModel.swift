c77cf93ac951de91aa5e2980efa470e9a8ab2e65
Your Name <you@example.com>
2023-06-17 01:23:33 -0700

    checkifuserlikedpost/unlikepost

--- swiftUIInstagram/ViewModel/Feed/FeedCellViewModel.swift
+++ swiftUIInstagram/ViewModel/Feed/FeedCellViewModel.swift
@@ -20,6 +20,7 @@ class FeedCellViewModel:ObservableObject {
     
     init(post:Post) {
         self.post = post
+        checkIfUserLikedPost()
     }
     func like() {
         guard let uid = AuthViewModel.shared.userSession?.uid else {return}
@@ -37,7 +38,34 @@ class FeedCellViewModel:ObservableObject {
         
     }
     
-    func unlike() {  print("Like Post")}
+    func unlike() {
+        guard post.likes > 0 else { return }
+        guard let uid = AuthViewModel.shared.userSession?.uid else {return}
+        guard let postId = self.post.id else {return}
+        
+        
+        COLLECTION_POSTS.document(postId).collection("post-likes").document(uid).delete { _ in
+            COLLECTION_USERS.document(uid).collection("user-likes")
+                .document(postId).delete{ _ in
+                    COLLECTION_POSTS.document(postId).updateData(["likes" : self.post.likes - 1])
+                    
+                    self.post.didLike = false
+                    self.post.likes -= 1
+                }
+        }
+        
+        print("Unliked Post")
+        
+        
+    }
     
-    func checkIfUserLikedPost() {}
+    func checkIfUserLikedPost() {
+        guard let uid = AuthViewModel.shared.userSession?.uid else {return}
+        guard let postId = self.post.id else { return }
+        COLLECTION_USERS.document(uid).collection("user-likes").document(postId)
+            .getDocument{ snapshot,  _ in
+                guard let didLike = snapshot?.exists else { return }
+                self.post.didLike = didLike
+            }
+    }
 }
